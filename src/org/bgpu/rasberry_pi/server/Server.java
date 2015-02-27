5f2dd0045fc2d753ebb8260ed41557eccc304ff7
Khadanovich Sergey <glamdringmetrandira@gmail.com>
2015-02-27 16:54:56 +0900

    изменен ConfigLoader.java

--- src/org/bgpu/rasberry_pi/server/Server.java
+++ src/org/bgpu/rasberry_pi/server/Server.java
@@ -6,15 +6,21 @@ import java.net.Socket;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bgpu.rasberry_pi.structs.ConfigLoader;
 
 public class Server {
 	
 	private static final Logger LOGGER = LogManager.getLogger(Server.class);
 
 	public static void main(String... args) throws ClassNotFoundException {
+		//устанавливаем конфиг
+//		ConfigLoader.setDestination((args.length == 0) ? "/etc/raspberry/config.properties": args[0]);
+		ConfigLoader.setDestination("/etc/raspberry/config.properties");
+		
 		Class.forName("org.bgpu.rasberry_pi.core.IskanderusController");
 		Class.forName("org.bgpu.rasberry_pi.structs.ScriptCollection");
 		
+
 		try {
 			LOGGER.info("Starting server...");
 			@SuppressWarnings("resource")
--- src/org/bgpu/rasberry_pi/structs/ConfigLoader.java
+++ src/org/bgpu/rasberry_pi/structs/ConfigLoader.java
@@ -4,24 +4,49 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * класс, отвечающий за загрузки конфигурационного файла
+ * @author bazinga
+ *
+ */
 public class ConfigLoader {
 
 	private Properties properties = new Properties();
 	
-	private static final ConfigLoader CONFIG_LOADER = new ConfigLoader();
+	private static ConfigLoader CONFIG_LOADER = null;
 	
+	/**
+	 * устанавливает пусть к файлу с конфигом
+	 * этот метод должен быть вызван, самым первым в методе main()
+	 * инициализация остальных классов зависит от того был ли загружен конфиг
+	 * @param configDestination
+	 */
+	public static void setDestination(String configDestination) {
+		CONFIG_LOADER = new ConfigLoader(configDestination);
+	}
+	
+	/**
+	 * возвращает объект ConfigLoader
+	 * @return
+	 */
 	public static ConfigLoader instance() {
 		return CONFIG_LOADER;
 	}
 	
-	private ConfigLoader() {
+	
+	private ConfigLoader(String pathToFile) {
 		try {
-			properties.load(new FileInputStream("/etc/raspberry/config.properties"));
+			properties.load(new FileInputStream(pathToFile));
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		}		
 	}
 	
+	/**
+	 * возвращает значение из конфига
+	 * @param key ключ
+	 * @return значение по ключу
+	 */
 	public String getValue(String key) {
 		return properties.getProperty(key);
 	}
